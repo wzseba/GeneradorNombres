c2de494ed53b6d2f7b935722cc48688fee28c6a7
sebastian <wzseba@gmail.com>
2025-04-17 21:25:23 -0300

    first commit

---
+++ src/main/java/generador/GeneradorNombres.java
@@ -0,0 +1,44 @@
+package generador;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
+
+import com.github.javafaker.Faker;
+
+public class GeneradorNombres {
+
+	public static void generadorNom() throws IOException {
+
+		int cantidadNombres = 5;
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter("output.in"))) {
+
+			writer.write(Integer.toString(cantidadNombres));
+			writer.newLine();
+
+			Random random = new Random();
+			Faker faker = new Faker();
+
+			for (int i = 0; i < cantidadNombres; i++) {
+				int id = random.nextInt(450) + 1;
+				String nombre = faker.name().firstName(); // Solo nombre
+				String apellido = faker.name().lastName(); // Solo apellido
+
+				writer.write(id + " " + nombre + " " + apellido);
+
+				if (i < cantidadNombres - 1) {
+					writer.newLine();
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) throws IOException {
+		generadorNom();
+	}
+}
